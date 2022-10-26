a133080427392fd17ce8153520cd0cbffb380dbd
AlexL <alexandar.langer@yahoo.com>
2022-10-26 23:51:04 +0200

    Port fix

--- src/app.js
+++ src/app.js
@@ -80,6 +80,6 @@ app.get("*", (req, res) => {
 });
 
 // Server config
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(port, () => {
+  console.log(`Listening on port port`);
 });
